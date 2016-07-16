044a36ebea87ae316b05bbc4c1e3a5a0c7a64595
Gabriel Carneiro <gabtemp02@yahoo.com.br>
2016-07-16 12:59:01 -0300

    Updating java version

--- config.fish
+++ config.fish
@@ -1,4 +1,8 @@
-eval (python -m virtualfish)
+eval (python3 -m virtualfish)
+
+if test ! -e /tmp/$USER
+    mkdir /tmp/$USER
+end
 
 # If not running interactively, do not do anything
-status --is-interactive; and byobu
+#status --is-interactive; and byobu
--- functions/java8.fish
+++ functions/java8.fish
@@ -1,3 +1,3 @@
 function java8 --description 'Adds java 8 and maven 3.3.9 to PATH'
-  java_version 1.8.0_77 3.3.9
+  java_version 1.8.0_91 3.3.9
 end
